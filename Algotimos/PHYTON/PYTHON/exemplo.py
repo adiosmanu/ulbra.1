78fb28f9b057984621491879e59d8be4ceb0dc05
adiosmanu <manuellalfarias@gmail.com>
2022-09-06 23:33:19 -0300

    surbir arquivos de algoritmos

---
+++ Algotimos/PHYTON/PYTHON/exemplo.py
@@ -0,0 +1,10 @@
+print ("hello word")
+name= input ("digite o seu nome \n")
+print(name)
+peso=float(input("digite o seu peso \n"))
+altura=float(input("digite sua altura \n"))
+imc=peso/(altura*altura)
+print(f'{name} seu imc é {imc}')
+
+
+
---
+++ Algotimos/PHYTON/PYTHON/exercicio.py
@@ -0,0 +1,104 @@
+# a=int(input("informe um numero:"))
+# b=int(input("informe outro numero:"))
+# c=int(input("informe mais um numero:"))
+# soma= (a+b+c)
+# media= (soma)/2
+# print(f'a soma entre os tres numeros é de {soma}')
+# print(f'a media entre os tres numeros é de {media}')
+
+# a=float(input("informe um numero"))
+# b=float(input("digite outro numero"))
+# c=float(input("informe outro numero"))
+# d=float(input("informe mais um numero"))
+# e=float(input("informe outro numero"))
+# media=(a+b+c+d+e)/5
+# print(f"a media entre os numeros é de {media}")
+
+# n1=int(input("insira um numero:"))
+# n2=int(input("insira outro numero:"))
+# n3=int(input("insira mais um numero:"))
+# multiplicacao=(n1*n2*n3)
+# soma=(n1+n2+n3)
+# subtracao=(n1-n2-n3)
+# soma_total=(multiplicacao+soma+subtracao)
+# print(f'o resultado da multiplicacao entre os tres numeros é {multiplicacao}')
+# print(f'o resultado da soma entre os tres numeros é de: {soma}')
+# print(f'o resultado da subtracao entre os tres numeros é de: {subtracao}')
+# print(f'a soma total dos resultados é de {soma_total}')
+
+# numero_vendedor=float(input("Insira o numero do vendedor:"))
+# salario_fixo=float(input("insira valor d salario"))
+# carros_vendidos=float(input("insira numero de carros vendidos:"))
+# valor_por_carro=float(input("insira valor que recebe por carro vendido:"))
+# salario_total=salario_fixo+(carros_vendidos*valor_por_carro)
+# print(f'o valor do salário junto com comissao do funcionario {numero_vendedor} é de R${salario_total}')
+
+# preco_fabr=float(input("Digite o valor de fabrica do computador:"))
+# imposto= preco_fabr*0.45
+# revenda=preco_fabr*0.28
+# preco_total=preco_fabr+imposto+revenda
+# print(f'O preco total do computador com impostos e revenda é de R${preco_total}')
+
+# a=int(input("Insira um valor para A:"))
+# b=int(input("Insira um valor para B:"))
+# c=int(input("Insira um valor para C:"))
+# media=(a+b+c)/3
+# print(f'O valor da média é de {media}')
+
+# x=int(input("Digite um valor:"))
+# y=int(input("Digite outro valor:"))
+# quociente=x/y
+# resto=x%y
+# print(f'O quociente de {x} e {y} é {quociente} e o resto é {resto}')
+
+# tempo_gasto=float(input("Digite o tempo gasto:"))
+# velocidade_media=float(input("Digite a velocidade média:"))
+# distancia=tempo_gasto*velocidade_media
+# litros_gastos=distancia/12
+# print(f'O valor de litros gastos na viagem é de {litros_gastos}')
+
+# preco_fabrica=float(input("Digite o valor de fabrica do computador:"))
+# imposto=preco_fabrica*0.30
+# revenda=preco_fabrica*0.10
+# preco_total=preco_fabrica+imposto+revenda
+# print(f'O valor final do computador com impostos e revenda é de R${preco_total}')
+
+# nota_ps=float(input("digite sua nota"))
+
+# if nota_ps >=6:
+#     print("voce foi aprovado!\n")
+#     print("parabens\n")
+# elif nota_ps<6 and nota_ps>4:
+#     print("voce pegou exame")
+#     print("sem pizza galera")
+# else:
+#     print("voce nao foi aprovado")
+
+# print(f'sua nota é {nota_ps}')
+
+# a=int(input("Digite um numero\n"))
+# b=int(input("Digite outro numero\n"))
+
+# if a > b:
+#     print(f'o valor final é  {a +100}')
+# else:
+#     print(f'o valor final é {b +100}')
+
+# pessoa=int(input("digite a idade da pessoa:\n"))
+
+# if pessoa >=18:
+#     print("Ela é maior de idade:")
+# else:
+#     print("Ela é menor de idade")
+
+
+
+
+
+
+
+ 
+
+
+
+
